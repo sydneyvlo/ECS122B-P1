aee9a012c7b2a6773375f14fca409798016e382c
Sydney Lo <sydneyvlo@gmail.com>
2016-04-27 20:55:28 -0700

    Got the scraping of the time output.

--- src/pipeline.sh
+++ src/pipeline.sh
@@ -5,6 +5,7 @@ if [ "$1" != "" ] && [ "$2" != "" ] && [ "3" != "" ]; then
 	echo "Command line arguments are $1 and $2 and $3"
 else
 	echo "Insufficient amount of command line arguments"
+fi
 
 if [[ "$1" == "quicksort" ]]; then
 	# Quicksort header file
@@ -14,14 +15,24 @@ else
 	echo '"Sample Number","Language","Time","Number of Recursive Calls","Number of Transitions","Number of Compares"' >> mergesort.csv
 fi
 
+version="${java -version}"
+
 # Generate the testing sample
-for (( numTest=1; numTest<=$1; numTest++))
+for (( numTest=1; numTest<=$2; numTest++))
 	do
 		for (( c=1; c<=$3; c++ ))
 			do
 			    echo $RANDOM >> "test$numTest.txt"
 			done
+		
+		# Gets the real time of the sorting algorithm.
+		exec 3>&1 4>&2
+		foo=$( { time java "$1" "test$numTest.txt" 1>&3 2>&4; } 2>&1 )
+		exec 3>&- 4>&-
+		real=${foo#*real}
+		real=${real%%s*}
+
+		# Run the sort algorithm and store it in variables
+		
 	done
-
-
 
